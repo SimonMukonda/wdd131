fb676aa5bb3b516d00700efe42fb0fa3390c3ecc
Simon Mukonda <simonmktonto@gmail.com>
2026-01-15 20:29:35 +0200

    week two

---
+++ week01/week02/scripts/bom.js
@@ -0,0 +1,36 @@
+// const input = document.querySelector('#favchap');
+// const button = document.querySelector('button');
+// const list = document.querySelector('list');
+// const li = document.createElement('li');
+// const deletButton = document.createElement('button');
+
+// li.textContent = input.value;
+// deletButton.textContent = '❌';
+// li.append(deletButton);
+// list.append(li);
+
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+    if (input.value.trim() !== '') {
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+
+        input.value = '';
+        input.focus();
+    }
+});
